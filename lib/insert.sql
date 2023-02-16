6468cd39457ab59e221a70ea9b47b98a138c7be3
Joanna <astronautnemo@gmail.com>
2023-02-16 15:34:33 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,33 @@
+-- INSERT data
+
+-- Mr Chocolate
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr Chocolate', 11, 'M', 'blond', 'Choleric', 1);
+
+-- Rowdy
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 26, 'F', 'brown', 'Melancholy', 0);
+
+-- Tabitha
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 3, 'F', 'blue-gray', 'Phlegmatic', 1);
+
+-- Sergeant Brown
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 5, 'M', 'white', 'Sanguine', 1);
+
+-- Melissa
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 25, 'F', 'cinnamon', 'Supine', 1);
+
+-- Grinch
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 15, 'M', 'green', 'Choleric', 0);
+
+-- Wendy
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 10, 'F', 'blue-grau', 'Choleric', 1);
+
+-- unnamed
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 23, 'F', 'white', 'Melancholy', 1);
